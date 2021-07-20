d39028588b0372094c2bb13e6bb063343ff4ef96
Vanessa Dengel <dengelva@phoenix.ims.uni-stuttgart.de>
2021-07-20 11:31:49 +0200

    script acoustic feature extraction

---
+++ acoustic_features.sh
@@ -0,0 +1,24 @@
+export TEAMLABDIR=/mount/studenten/arbeitsdaten-studenten1/team-lab-phonetics/2021/student_directories/Vanessa_Dengel
+ 
+cd $TEAMLABDIR
+ 
+cd kaldi/egs/teamlab_pronunciation/s5
+
+ 
+. ./path.sh
+. ./cmd.sh
+ 
+utils/fix_data_dir.sh data/train_teamlab
+utils/fix_data_dir.sh data/dev_teamlab
+ 
+nj=38
+mfccdir=mfcc
+ 
+# edit conf/mfcc.conf to allow upsampling (run 'compute-mfcc-feats' for more information)
+ 
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/train_teamlab exp/make_mfcc/train_teamlab $mfccdir
+
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" data/dev_teamlab exp/make_mfcc/dev_teamlab $mfccdir
+ 
+steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train_teamlab $mfccdir
+steps/compute_cmvn_stats.sh data/dev exp/make_mfcc/dev_teamlab $mfccdir
